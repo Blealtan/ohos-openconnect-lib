ed81ae515756a3e0cbeb9ba3a6412418a26e356d
Huanqi Cao <blealtan@outlook.com>
2025-06-29 23:18:15 +0800

    Minimal compilation on ohos.

---
+++ config.h
@@ -0,0 +1,279 @@
+/* Generated and adjusted by hands for OpenHarmony OS. */
+
+/* External browser executable */
+/* #undef DEFAULT_EXTERNAL_BROWSER */
+
+/* p11-kit proxy */
+/* #undef DEFAULT_PKCS11_MODULE */
+
+/* The GnuTLS priority string */
+/* #undef DEFAULT_PRIO */
+
+/* Default vpnc-script locatin */
+#define DEFAULT_VPNCSCRIPT "/etc/vpnc/vpnc-script"
+
+/* Enable NLS support */
+/* #undef ENABLE_NLS */
+
+/* endian header include path */
+#define ENDIAN_HDR <endian.h>
+
+/* GSSAPI header */
+/* #undef GSSAPI_HDR */
+
+/* MinGW has afunix.h */
+/* #undef HAVE_AF_UNIX_H */
+
+/* Have alloca.h */
+#define HAVE_ALLOCA_H 1
+
+/* Have asprintf() function */
+#define HAVE_ASPRINTF 1
+
+/* OpenSSL has BIO_meth_free() function */
+#define HAVE_BIO_METH_FREE 1
+
+/* Have __builtin_clz() */
+#define HAVE_BUILTIN_CLZ 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Build with DTLS support */
+#define HAVE_DTLS 1
+
+/* OpenSSL has DTLS_client_method() function */
+#define HAVE_DTLS12 1
+
+/* OpenSSL has dtls1_stop_timer() function */
+/* #undef HAVE_DTLS1_STOP_TIMER */
+
+/* OpenSSL has ENGINE support */
+#define HAVE_ENGINE 1
+
+/* Have epoll */
+#define HAVE_EPOLL 1
+
+/* Build with ESP support */
+#define HAVE_ESP 1
+
+/* Have explicit_bzero() function */
+/* #undef HAVE_EXPLICIT_BZERO */
+
+/* Have explicit_memset() function */
+/* #undef HAVE_EXPLICIT_MEMSET */
+
+/* Have fdevname_r() function */
+/* #undef HAVE_FDEVNAME_R */
+
+/* MinGW declares getenv_s */
+/* #undef HAVE_GETENV_S_DECL */
+
+/* Have getline() function */
+#define HAVE_GETLINE 1
+
+/* From GnuTLS 3.4.0 */
+/* #undef HAVE_GNUTLS_SYSTEM_KEYS */
+
+/* Have GSSAPI support */
+/* #undef HAVE_GSSAPI */
+
+/* Support Cisco external browser HPKE (ECDH+HKDF+AES-256-GCM) */
+#define HAVE_HPKE_SUPPORT 1
+
+/* Have iconv() function */
+#define HAVE_ICONV 1
+
+/* Have inet_aton() */
+#define HAVE_INET_ATON 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+/* #undef HAVE_INTTYPES_H */
+
+/* Have IPV6_PATHMTU socket option */
+#define HAVE_IPV6_PATHMTU 1
+
+/* Have builtin json-parser package */
+#define HAVE_JSON 1
+
+/* Define to 1 if you have the `log' library (-llog). */
+/* #undef HAVE_LIBLOG */
+
+/* Define to 1 if you have the `nsl' library (-lnsl). */
+/* #undef HAVE_LIBNSL */
+
+/* Have libp11 and p11-kit for OpenSSL */
+/* #undef HAVE_LIBP11 */
+
+/* Have libpcsclite */
+/* #undef HAVE_LIBPCSCLITE */
+
+/* Have libpskc */
+/* #undef HAVE_LIBPSKC */
+
+/* Define to 1 if you have the `socket' library (-lsocket). */
+/* #undef HAVE_LIBSOCKET */
+
+/* Have libstoken */
+/* #undef HAVE_LIBSTOKEN */
+
+/* Have localtime_r() function */
+#define HAVE_LOCALTIME_R 1
+
+/* Have localtime_s() function */
+/* #undef HAVE_LOCALTIME_S */
+
+/* LZ4 was found */
+/* #undef HAVE_LZ4 */
+
+/* From LZ4 r129 */
+/* #undef HAVE_LZ4_COMPRESS_DEFAULT */
+
+/* Have memset_s() function */
+/* #undef HAVE_MEMSET_S */
+
+/* Have net/if_utun.h */
+/* #undef HAVE_NET_UTUN_H */
+
+/* Have nl_langinfo() function */
+#define HAVE_NL_LANGINFO 1
+
+/* Have. P11. Kit. */
+/* #undef HAVE_P11KIT */
+
+/* Have posix_spawn() function */
+#define HAVE_POSIX_SPAWN 1
+
+/* MinGW declares _putenv_s */
+/* #undef HAVE_PUTENV_S_DECL */
+
+/* OpenSSL has SSL_CIPHER_find() function */
+#define HAVE_SSL_CIPHER_FIND 1
+
+/* OpenSSL has SSL_CTX_set_min_proto_version() function */
+#define HAVE_SSL_CTX_PROTOVER 1
+
+/* Have statfs() function */
+#define HAVE_STATFS 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+/* #undef HAVE_STDINT_H */
+
+/* Define to 1 if you have the <stdio.h> header file. */
+/* #undef HAVE_STDIO_H */
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+/* #undef HAVE_STDLIB_H */
+
+/* Have strcasestr() function */
+#define HAVE_STRCASESTR 1
+
+/* Have strchrnul() function */
+#define HAVE_STRCHRNUL 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+/* #undef HAVE_STRINGS_H */
+
+/* Define to 1 if you have the <string.h> header file. */
+/* #undef HAVE_STRING_H */
+
+/* Have strndup() function */
+#define HAVE_STRNDUP 1
+
+/* On SunOS time() can go backwards */
+/* #undef HAVE_SUNOS_BROKEN_TIME */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+/* #undef HAVE_SYS_STAT_H */
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+/* #undef HAVE_SYS_TYPES_H */
+
+/* Have Trousers TSS library */
+/* #undef HAVE_TROUSERS */
+
+/* TSS2 library */
+/* #undef HAVE_TSS2 */
+
+/* Define to 1 if you have the <unistd.h> header file. */
+/* #undef HAVE_UNISTD_H */
+
+/* Have vasprintf() function */
+#define HAVE_VASPRINTF 1
+
+/* Have va_copy() */
+/* #undef HAVE_VA_COPY */
+
+/* Have vhost */
+#define HAVE_VHOST 1
+
+/* Have __va_copy() */
+/* #undef HAVE___VA_COPY */
+
+/* Define as const if the declaration of iconv() needs const. */
+#define ICONV_CONST 
+
+/* if_tun.h include path */
+#define IF_TUN_HDR "linux/if_tun.h"
+
+/* libproxy header file */
+/* #undef LIBPROXY_HDR */
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Try to make getenv_s and _putenv_s available */
+/* #undef MINGW_HAS_SECURE_API */
+
+/* Using GnuTLS */
+/* #undef OPENCONNECT_GNUTLS */
+
+/* Using OpenSSL */
+#define OPENCONNECT_OPENSSL 1
+
+/* We need to update to OpenSSL 3.0.0 API */
+#define OPENSSL_SUPPRESS_DEPRECATED 1
+
+/* Name of package */
+#define PACKAGE "openconnect"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "openconnect"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "openconnect 9.12"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "openconnect"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "9.12"
+
+/* Define to 1 if all of the C90 standard headers exist (not just the ones
+   required in a freestanding environment). This macro is provided for
+   backward compatibility; new code need not use it. */
+/* #undef STDC_HEADERS */
+
+/* Version number of package */
+#define VERSION "9.12"
+
+/* _GNU_SOURCE */
+#define _GNU_SOURCE 1
+
+/* _NETBSD_SOURCE */
+/* #undef _NETBSD_SOURCE */
+
+/* _POSIX_C_SOURCE */
+/* #undef _POSIX_C_SOURCE */
+
+/* Windows API version */
+/* #undef _WIN32_WINNT */
+
+/* To request memset_s */
+/* #undef __STDC_WANT_LIB_EXT1__ */
